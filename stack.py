597f6bef494aa5139fcd6a975e7399e9c4c565ed
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-21 08:30:45 -0300

    Update stack.py
    Added push(value) method.

--- stack.py
+++ stack.py
@@ -14,7 +14,13 @@ class Stack:
         while temp is not None:
             print(temp.value)
             temp = temp.next
-        
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+        self.height += 1
+
 my_stack = Stack(4)
 
 my_stack.print_stack()
